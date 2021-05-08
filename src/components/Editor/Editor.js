8b97fa64f3dbab0492d354e91d4d4aa961e9cda2
minwooKim <vvee1253@gmail.com>
2021-05-08 21:01:37 +0900

    Add Sidebar to Editor page

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const EditorContainer = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  width: 100vw;
-  height: 100vh;
+  max-width: 800px;
+  width: 100%;
+  padding: 0 35px;
+  box-sizing: border-box;
 `;
 
 const Main = styled.main`
@@ -28,8 +27,11 @@ const Sidebar = styled.div`
 
 const Selectbox = styled.div`
   height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   box-sizing: border-box;
-  padding: 0 0 0 20px;
+  padding: 0 1.3rem 0 1.3rem;
   background: #eee;
   line-height: 40px !important;
   font-size: 14px;
@@ -42,7 +44,7 @@ const SidebarToggler = styled.div`
   width: 40px;
   height: 40px;
   top: 170px;
-  left: -40px;
+  left: -41px;
   background: #fff;
   border: 1px solid rgba(211, 211, 211, 0.52);
   border-right: none;
@@ -52,6 +54,31 @@ const SidebarToggler = styled.div`
   cursor: pointer;
 `;
 
+const BlockList = styled.div`
+  height: 100%;
+  margin: 0;
+  padding: 0 0 50px;
+  box-sizing: border-box;
+  overflow-y: auto;
+  overflow-x: hidden;
+  text-align: center;
+`;
+
+const BlockItem = styled.div`
+  width: 170px;
+  overflow: hidden;
+  background: rgb(0, 0, 0, 0.5);
+  margin: 22px 22px 0;
+  cursor: move;
+  outline: rgba(219, 219, 219, 0.52) 1px solid;
+  box-shadow: 0 5px 15px rgb(0 0 0 / 3%);
+
+  & img {
+    width: 100%;
+    display: block;
+  }
+`;
+
 export default function Editor() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
@@ -59,12 +86,55 @@ export default function Editor() {
     <EditorContainer>
       <Main />
       <Sidebar isSidebarOpen={isSidebarOpen}>
-        <Selectbox>
-          Basic
-        </Selectbox>
         <SidebarToggler onClick={() => setIsSidebarOpen((prev) => !prev)}>
-          {isSidebarOpen ? '>' : '<'}
+          {isSidebarOpen ? '﹥' : '﹤'}
         </SidebarToggler>
+        <Selectbox>
+          <span>
+            Basic
+          </span>
+          <span>
+            ▼
+          </span>
+        </Selectbox>
+        <BlockList>
+          <BlockItem>
+            <img id='title1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-01.png' alt='title' />
+          </BlockItem>
+          <BlockItem>
+            <img id='img1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-05.png' alt='img' />
+          </BlockItem>
+          <BlockItem>
+            <img id='video1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/element-video.png' alt='video' />
+          </BlockItem>
+          <BlockItem>
+            <img id='social1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-16.png' alt='social' />
+          </BlockItem>
+          <BlockItem>
+            <img id='title1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-01.png' alt='title' />
+          </BlockItem>
+          <BlockItem>
+            <img id='img1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-05.png' alt='img' />
+          </BlockItem>
+          <BlockItem>
+            <img id='video1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/element-video.png' alt='video' />
+          </BlockItem>
+          <BlockItem>
+            <img id='social1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-16.png' alt='social' />
+          </BlockItem>
+          <BlockItem>
+            <img id='title1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-01.png' alt='title' />
+          </BlockItem>
+          <BlockItem>
+            <img id='img1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-05.png' alt='img' />
+          </BlockItem>
+          <BlockItem>
+            <img id='video1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/element-video.png' alt='video' />
+          </BlockItem>
+          <BlockItem>
+            <img id='social1' src='https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-16.png' alt='social' />
+          </BlockItem>
+        </BlockList>
       </Sidebar>
     </EditorContainer>
   );
