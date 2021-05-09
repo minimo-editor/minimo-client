6792ec0c56b1aa032f2e094837d32b6f6877f5b1
minwooKim <vvee1253@gmail.com>
2021-05-09 16:52:13 +0900

    Add ghost to blocksMap

--- src/utils/blocksMap.js
+++ src/utils/blocksMap.js
@@ -1,12 +1,13 @@
 import Img from '../components/Blocks/Img';
 import Title from '../components/Blocks/Title';
 import Video from '../components/Blocks/Video';
+import Ghost from '../components/Blocks/Ghost';
 
 const blocksMap = new Map();
 
 blocksMap
   .set('title1', Title)
   .set('img1', Img)
-  .set('video1', Video);
-
+  .set('video1', Video)
+  .set('ghost', Ghost);
 export default blocksMap;
