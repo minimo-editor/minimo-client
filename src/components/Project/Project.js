4b5e823c37150bb6fd2fb92bd15feb57a47f8c5f
minwooKim <vvee1253@gmail.com>
2021-05-09 22:50:48 +0900

    Add drag and drop between blocks, need to fix bugs

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,7 +1,120 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import * as ICON from 'react-feather';
 import blocksMap from '../../utils/blocksMap';
+
+function Tools({ makeDraggable }) {
+  return (
+    <ToolWrapper>
+      <MoveTool
+        onMouseDown={makeDraggable}
+      >
+        <ICON.Move size={19} />
+      </MoveTool>
+      <RemoveTool>
+        <ICON.X size={19} />
+      </RemoveTool>
+    </ToolWrapper>
+  );
+}
+
+export default function Project({
+  blocks,
+  setBlocks,
+  onDrop,
+  onDragEnter,
+  onDragLeave,
+  handleChangeBlock,
+}) {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [draggingIndex, setDraggingIndex] = useState(null);
+
+  const dragOverItem = useRef();
+  const draggingItem = useRef();
+
+  function onClick(e, index) {
+    // TODO: make it null when clicked background
+    // json에서 selected로... 관리해야 할 수도...?
+    setActiveIndex(index);
+    setDraggingIndex(null);
+  }
+
+  function handleDragStart(e, index) {
+    draggingItem.current = index;
+  }
+
+  function handleDragEnter(e, index) {
+    if (draggingItem.current === index) {
+      return;
+    }
+
+    if (draggingItem.current === null) {
+      onDragEnter(e, index);
+      return;
+    }
+
+    dragOverItem.current = index;
+    const blocksCopy = [...blocks];
+
+    const draggingItemContent = blocksCopy[draggingItem.current];
+
+    blocksCopy.splice(draggingItem.current, 1);
+    blocksCopy.splice(dragOverItem.current, 0, draggingItemContent);
+
+    draggingItem.current = dragOverItem.current;
+    dragOverItem.current = null;
+    setBlocks(blocksCopy);
+    setActiveIndex(dragOverItem.current);
+  }
+
+  return (
+    <div>
+      {blocks.map((block, index) => {
+        // TODO: 질문, 너무 말이 안되는 구현인가?
+        const Block = blocksMap.get(block.type);
+        const isActive = index === activeIndex;
+        const isDragging = index === draggingIndex;
+
+        return (
+          <BlockWrapper
+            onDragEnterCapture={(e) => handleDragEnter(e, index)}
+            onDragLeave={onDragLeave}
+            onDrop={(e) => onDrop(e, index)}
+            onDragOver={(e) => e.preventDefault()}
+            isActive={isActive}
+            onClick={(e) => onClick(e, index)}
+            draggable={isDragging}
+            onDragStart={(e) => handleDragStart(e, index)}
+          >
+            <Block
+              index={index}
+              data={block.data}
+              onChange={handleChangeBlock}
+            />
+            {isActive && (
+              <Tools
+                makeDraggable={() => setDraggingIndex(index)}
+              />
+            )}
+          </BlockWrapper>
+        );
+      })}
+    </div>
+  );
+}
+
+// function BlockContainer({ children }) {
+//   const [isActive, setIsActive] = useState(false);
+
+//   return (
+//     <Wrapper
+//       isActive={isActive}
+//       onClick={() => setIsActive((prev) => !prev)}
+//     >
+//       {children}
+//     </Wrapper>
+//   );
+// }
 
 const BlockWrapper = styled.div`
   position: relative;
@@ -37,73 +150,3 @@ const RemoveTool = styled.div`
   cursor: pointer;
   background: rgba(255, 85, 4, 0.9);
 `;
-
-// function BlockContainer({ children }) {
-//   const [isActive, setIsActive] = useState(false);
-
-//   return (
-//     <Wrapper
-//       isActive={isActive}
-//       onClick={() => setIsActive((prev) => !prev)}
-//     >
-//       {children}
-//     </Wrapper>
-//   );
-// }
-
-function Tool() {
-  return (
-    <ToolWrapper>
-      <MoveTool>
-        <ICON.Move size={20} />
-      </MoveTool>
-      <RemoveTool>
-        <ICON.X size={20} />
-      </RemoveTool>
-    </ToolWrapper>
-  );
-}
-
-export default function Project({
-  blocks,
-  isEditable,
-  onDrop,
-  onDragEnter,
-  onDragLeave,
-  handleClickBlock,
-  handleChangeBlock,
-}) {
-  const [activeIndex, setActiveIndex] = useState(null);
-
-  function onClick(e, index) {
-    setActiveIndex(index);
-  }
-
-  return (
-    <div>
-      {blocks.map((block, index) => {
-        const Block = blocksMap.get(block.type);
-        const isActive = index === activeIndex;
-        return (
-          <BlockWrapper
-            onDragEnterCapture={(e) => onDragEnter(e, index)}
-            onDragLeave={onDragLeave}
-            onDrop={(e) => onDrop(e, index)}
-            onDragOver={(e) => e.preventDefault()}
-            isActive={isActive}
-            onClick={(e) => onClick(e, index)}
-          >
-            <Block
-              index={index}
-              data={block.data}
-              onChange={handleChangeBlock}
-            />
-            {isActive && (
-              <Tool />
-            )}
-          </BlockWrapper>
-        );
-      })}
-    </div>
-  );
-}
