cb37e3b91e04296000d378af3e7887b4637fe2af
minwooKim <vvee1253@gmail.com>
2021-05-10 19:16:53 +0900

    Add socials icons

--- src/components/Blocks/Socials/Socials.js
+++ src/components/Blocks/Socials/Socials.js
@@ -2,12 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 import * as ICON from 'react-feather';
 
-export default function Socials() {
+export default function Socials({
+  index,
+  data,
+  onChange,
+}) {
+  const { facebookLink, twitterLink, youtubeLink } = data;
   return (
     <SocialsContainer>
-      <ICON.Facebook />
-      <ICON.Twitter />
-      <ICON.Youtube />
+      <SocialIcon
+        target='_blank'
+        bgColor='#408bdb'
+        href={facebookLink || 'https://www.facebook.com/'}
+      >
+        <ICON.Facebook fill='white' />
+      </SocialIcon>
+      <SocialIcon
+        target='_blank'
+        href={twitterLink || 'https://twitter.com/?lang=en'}
+        bgColor='#52bfff'
+      >
+        <ICON.Twitter fill='white' />
+      </SocialIcon>
+      <SocialIcon
+        target='_blank'
+        href={youtubeLink || 'https://youtube.com/?lang=en'}
+        bgColor='#df3220'
+      >
+        <ICON.Youtube fill='white' />
+      </SocialIcon>
     </SocialsContainer>
   );
 }
@@ -15,5 +38,16 @@ export default function Socials() {
 const SocialsContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-around;
+  align-items: center;
+  width: 50%;
+  margin: auto;
+`;
+
+const SocialIcon = styled.a`
+  background: ${({ bgColor }) => bgColor ?? 'white'};
+  border-radius: 50%;
+  padding: 1rem;
+  line-height: 0;
+  cursor: pointer;
 `;
--- src/utils/blocksMap.js
+++ src/utils/blocksMap.js
@@ -2,6 +2,7 @@ import Img from '../components/Blocks/Img';
 import Title from '../components/Blocks/Title';
 import Video from '../components/Blocks/Video';
 import Ghost from '../components/Blocks/Ghost';
+import Socials from '../components/Blocks/Socials';
 
 const blocksMap = new Map();
 
@@ -9,5 +10,7 @@ blocksMap
   .set('title1', Title)
   .set('img1', Img)
   .set('video1', Video)
+  .set('social1', Socials)
   .set('ghost', Ghost);
+
 export default blocksMap;
