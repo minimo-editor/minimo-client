8bf88c39af924f111ab3f7e9d685618d7df5cfb9
minwooKim <vvee1253@gmail.com>
2021-05-10 17:13:03 +0900

    Add blockImgLIst constant

---
+++ src/constants/blockImgList.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 'title1',
+    src: 'https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-01.png',
+  },
+  {
+    id: 'img1',
+    src: 'https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-05.png',
+  },
+  {
+    id: 'video1',
+    src: 'https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/element-video.png',
+  },
+  {
+    id: 'social1',
+    src: 'https://innovastudio.com/builderdemo/assets/minimalist-blocks/preview/basic-16.png',
+  },
+];
