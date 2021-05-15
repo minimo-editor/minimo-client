68d29e4acc6dfb79ab6b26f16d1f30a3f575a1a3
minwooKim <vvee1253@gmail.com>
2021-05-15 11:19:11 +0900

    Add firebase config

---
+++ src/apis/firebase.config.js
@@ -0,0 +1,9 @@
+// TODO: firebase/app 과의 차이 알아보기
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+});
+
+export default firebase;
