e5d95e9ba68c4cc934668f2bb0591dd12aae514c
minwooKim <vvee1253@gmail.com>
2021-05-15 10:41:51 +0900

    Add authReducer

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,21 @@
+export const TYPES = {
+  AUTH_TRY: 'AUTH_TRY',
+  AUTH_SUCCESS: 'AUTH_SUCCESS',
+  AUTH_ERROR: 'AUTH_ERROR',
+  AUTH_CLEAN_UP: 'AUTH_CLEAN_UP',
+};
+
+export default function authReducer(state, action) {
+  switch (action.type) {
+    case TYPES.AUTH_TRY:
+      return { ...state, isLoading: true };
+    case TYPES.AUTH_SUCCESS:
+      return { ...state, isLoading: false };
+    case TYPES.AUTH_ERROR:
+      return { ...state, isLoading: false, error: action.payload };
+    case TYPES.AUTH_CLEAN_UP:
+      return { ...state, isLoading: false, error: null };
+    default:
+      return state;
+  }
+}
