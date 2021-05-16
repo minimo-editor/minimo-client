a8b41716f3482eade514a21cc960e49d7ec6687d
minwooKim <vvee1253@gmail.com>
2021-05-16 15:53:51 +0900

    Add publish form style

--- src/components/Publish/Publish.js
+++ src/components/Publish/Publish.js
@@ -1,7 +1,11 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, {
+  useContext, useRef, useState,
+} from 'react';
 import styled from 'styled-components';
 import { checkValidAddress, postProject } from '../../apis/project';
 import { ProjectContext } from '../../contexts/ProjectContext';
+
+// TODO: validation ***이곳 매우 중요
 
 export default function Publish() {
   const { project, setProject } = useContext(ProjectContext);
@@ -40,11 +44,19 @@ export default function Publish() {
 
   return (
     <Container>
-      <form
+      <FormContainer
         onSubmit={onSubmit}
       >
-        <label>
-          make your own address
+        <Label>
+          Project name
+          <input
+            ref={addressRef}
+            required
+            type='text'
+          />
+        </Label>
+        <Label>
+          Address
           <input
             ref={addressRef}
             required
@@ -56,14 +68,14 @@ export default function Publish() {
           >
             check
           </button>
-        </label>
+        </Label>
         <button
           disabled={!isAddressValid}
           type='submit'
         >
           publish
         </button>
-      </form>
+      </FormContainer>
       <div>
         <p>{result ? 'success' : 'fail'}</p>
         <p>{project.address}</p>
@@ -74,5 +86,77 @@ export default function Publish() {
 
 const Container = styled.div`
   margin: auto;
-  width: fit-content;
+  background: #fff;
+  position: relative;
+  border-radius: 2px;
+  padding: 2rem;
+  width: 550px;
+  box-shadow: 
+    0 16px 24px 2px rgba(0, 0, 0, 0.14),
+    0 6px 30px 5px rgba(0, 0, 0, 0.12),
+    0 8px 10px -5px rgba(0, 0, 0, 0.3);
+  transition: transform .1s ease-in-out;
+`;
+
+const InputContainer = styled.div`
+  position: relative;
+  opacity: 0;
+  width: 350px;
+  height: 100px;
+  margin-top: 25px;
+  transition: opacity .3s ease-in-out;
+`;
+
+const InputLabel = styled.label`
+  position: absolute;
+  pointer-events: none;
+  top: 0;
+  left: 0;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 0 5px;
+  transition: .2s ease-in-out;
+`;
+
+const InputField = styled.input`
+  width: 100%;
+  padding: 0 5px;
+  border: none;
+  font-size: 20px;
+  font-weight: bold;
+  outline: 0;
+  background: transparent;
+  box-shadow:none;
+`;
+
+const InputProgress = styled.div`
+  position: absolute;
+  border-bottom: 2px solid #25a3ff;
+  padding: 3px 0;
+  width: 0;
+  transition: width .6s ease-in-out;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 0 5px;
+
+  & input {
+    width: 100%;
+  }
+`;
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: auto;
 `;
