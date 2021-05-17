a3442e5f8f16f25c207f1b417315dd72422b7347
minwooKim <vvee1253@gmail.com>
2021-05-17 16:49:53 +0900

    Change import style

--- src/contexts/ProjectContext.js
+++ src/contexts/ProjectContext.js
@@ -1,7 +1,4 @@
-import React, {
-  createContext,
-  useState,
-} from 'react';
+import React, { createContext, useState } from 'react';
 
 export const ProjectContext = createContext();
 // TODO: steps 로직이 분리되어있음, 매니저와 합치고, 스탭 관련은 스탭바가 가지고 있던, 스탭바 콘텍스트를 만들 던 해야할 듯.
