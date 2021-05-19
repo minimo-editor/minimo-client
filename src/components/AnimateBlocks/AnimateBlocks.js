83470dfeae3c6c10d8672816229a6eb0a4103254
minwooKim <vvee1253@gmail.com>
2021-05-19 14:03:58 +0900

    Add AnimateBlock component

---
+++ src/components/AnimateBlocks/AnimateBlocks.js
@@ -0,0 +1,55 @@
+import {
+  useEffect,
+  useState,
+  useLayoutEffect,
+  Children,
+} from 'react';
+import usePrevious from '../../hooks/usePrevious';
+import calculateBoundingBoxes from '../../utils/calculateBoundingBoxes';
+
+export default function AnimateBlocks({ children }) {
+  const [boundingBox, setBoundingBox] = useState({});
+  const [prevBoundingBox, setPrevBoundingBox] = useState({});
+  const prevChildren = usePrevious(children);
+
+  useLayoutEffect(() => {
+    const newBoundingBox = calculateBoundingBoxes(children);
+    setBoundingBox(newBoundingBox);
+  }, [children]);
+
+  useLayoutEffect(() => {
+    // TODO : rename
+    const prevBoundBox = calculateBoundingBoxes(prevChildren);
+    setPrevBoundingBox(prevBoundBox);
+  }, [prevChildren]);
+
+  useEffect(() => {
+    const hasPrevBoundingBox = Object.keys(prevBoundingBox).length;
+
+    if (hasPrevBoundingBox) {
+      Children.forEach(children, (child) => {
+        const domNode = child.ref.current;
+        const firstBox = prevBoundingBox[child.key];
+        const lastBox = boundingBox[child.key];
+        const changeInY = firstBox.top - lastBox.top;
+
+        if (changeInY) {
+          requestAnimationFrame(() => {
+            // NOTE: Before the DOM paints, invert child to old position
+            domNode.style.transform = `translateY(${changeInY}px)`;
+            domNode.style.transition = 'transform 0s';
+
+            requestAnimationFrame(() => {
+              // NOTE: After the previous frame, remove
+              // the transistion to play the animation
+              domNode.style.transform = '';
+              domNode.style.transition = 'transform 700ms';
+            });
+          });
+        }
+      });
+    }
+  }, [boundingBox, prevBoundingBox, children]);
+
+  return children;
+}
