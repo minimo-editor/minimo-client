ecd4c2694740cb6cd2539b003c05e87dffdba3b1
minwooKim <vvee1253@gmail.com>
2021-05-20 09:25:00 +0900

    Add S3config

---
+++ src/configs/S3Config.js
@@ -0,0 +1,7 @@
+export default {
+  bucketName: 'minimo',
+  dirName: 'project-image',
+  region: 'ap-northeast-2',
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
+};
