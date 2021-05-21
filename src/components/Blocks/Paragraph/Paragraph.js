003b5d9bb744191f6cdfe69cc91b27aa2ccc13e5
minwooKim <vvee1253@gmail.com>
2021-05-21 20:49:44 +0900

    Add color picker to paragraph

--- src/components/Blocks/Paragraph/Paragraph.js
+++ src/components/Blocks/Paragraph/Paragraph.js
@@ -3,25 +3,37 @@ import styled from 'styled-components';
 import ContentEditable from 'react-contenteditable';
 import getDefaultParagraph from './defaultData';
 import isEmptyObject from '../../../utils/isEmptyObject';
+import useColorPicker from '../../../hooks/useColorPicker';
+import { TextColorPicker } from '../../shared/ColorPicker';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  position: relative;
+  text-align: center;
   width: 100%;
-  color: black;
-  font-weight: bolder;
   font-size: 1rem;
 `;
 
-export default function Title({
+export default function Paragragh({
   data,
   index,
   isEditable = true,
+  isActive,
   onChange,
+  handleChangeStyle,
 }) {
   // NOTE: 필요한가? editor에서 주기는 한다.
+  const {
+    color,
+    isColorPickerOpen,
+    toggleColorPicker,
+    handleChangeColor,
+  } = useColorPicker();
+
+  function onChangeColor(newColor) {
+    handleChangeColor(newColor);
+    handleChangeStyle(newColor.hex, index, 'color');
+  }
+
   const titleData = isEmptyObject(data) ? getDefaultParagraph() : data;
   const { texts } = titleData.contents;
   const { styles } = titleData;
@@ -34,6 +46,14 @@ export default function Title({
         onChange={(e) => onChange(e, index, 'texts')}
         disabled={!isEditable}
       />
+      {isActive && (
+        <TextColorPicker
+          color={color}
+          onChange={onChangeColor}
+          toggleColorPicker={toggleColorPicker}
+          isColorPickerOpen={isColorPickerOpen}
+        />
+      )}
     </Container>
   );
 }
