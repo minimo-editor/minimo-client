52333902e6dcc23927551c932b21f2b2ec949d0b
minwooKim <vvee1253@gmail.com>
2021-05-27 17:21:15 +0900

    Rename projectStepBar to ProjectFormStepper

---
+++ src/components/ProjectFormStepper/ProjectFormStepper.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+import * as ICON from 'react-feather';
+import Editor from '../Editor/Editor';
+import Preview from '../Preview/Preview';
+import Publish from '../Publish';
+import Templates from '../Templates';
+import Stepper from '../shared/Stepper';
+import useStepper from '../../hooks/useStepper';
+
+const STEPS = [
+  {
+    step: 0,
+    label: 'Select a template',
+    content: Templates,
+  },
+  {
+    step: 1,
+    label: 'Edit your project',
+    content: Editor,
+  },
+  {
+    step: 2,
+    label: 'Preview',
+    content: Preview,
+  },
+  {
+    step: 3,
+    label: 'Publish',
+    content: Publish,
+  },
+];
+
+export default function ProjectFormStepper() {
+  const { currentStep, moveToNextStep, moveToPrevStep } = useStepper(STEPS.length);
+  const Content = STEPS[currentStep].content;
+
+  return (
+    <>
+      <StepperContainer>
+        <Button onClick={moveToPrevStep}>
+          <ICON.ArrowLeftCircle color='white' />
+        </Button>
+        <Stepper
+          steps={STEPS}
+          currentStep={currentStep}
+        />
+        <Button onClick={moveToNextStep}>
+          <ICON.ArrowRightCircle color='white' />
+        </Button>
+      </StepperContainer>
+      <Content />
+    </>
+  );
+}
+
+const Button = styled.button`
+  all: unset;
+  border-radius: 50%;
+  background-color: grey;
+  cursor: pointer;
+
+  &:hover {
+    background-color: darkgray;
+  }
+`;
+
+const StepperContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
