4a08aba5261524dc043b0552cd262fa3543095ce
minwooKim <vvee1253@gmail.com>
2021-06-08 21:08:19 +0900

    Delete unused parameter

--- src/hooks/useImmutableReducer.js
+++ src/hooks/useImmutableReducer.js
@@ -4,9 +4,7 @@ import { useMemo, useReducer } from 'react';
 export default function useImmerReducer(
   reducer,
   initialState,
-  action,
 ) {
   const cachedReducer = useMemo(() => produce(reducer), [reducer]);
-
-  return useReducer(cachedReducer, initialState, action);
+  return useReducer(cachedReducer, initialState);
 }
