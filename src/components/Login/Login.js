a576f3e171c0634db5e009cb25f09d35d937b704
minwooKim <vvee1253@gmail.com>
2021-06-10 17:29:37 +0900

    Add test: Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,30 +3,17 @@ import firebase from 'firebase/app';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { Redirect } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
+import firebaseUiConfig from '../../configs/firebaseUi';
 
 export default function Login() {
   const { user } = useContext(AuthContext);
-
-  const uiConfig = {
-    signInFlow: 'popup',
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: () => false,
-    },
-  };
 
   return (
     user
       ? <Redirect to={{ pathname: '/editor' }} />
       : (
         <StyledFirebaseAuth
-          uiConfig={uiConfig}
+          uiConfig={firebaseUiConfig}
           firebaseAuth={firebase.auth()}
         />
       )
---
+++ src/components/Login/Login.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Login from './Login';
+import { AuthProvider } from '../../contexts/AuthContext';
+import '@testing-library/jest-dom/extend-expect';
+
+test('should display login ui when not logged in.', () => {
+  render(
+    <AuthProvider value={{ user: null }}>
+      <Login />
+    </AuthProvider>,
+  );
+
+  const firebaseLogin = document.getElementById('firebaseui_container');
+  expect(firebaseLogin).toBeInTheDocument();
+});
