644ad7e60e65316a66912214fa48e8506ae682d0
minwooKim <vvee1253@gmail.com>
2021-06-10 16:32:39 +0900

    Add test: App component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,22 +1,20 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-// import { AuthContext } from '../../contexts/AuthContext';
-// import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+import { AuthProvider } from '../../contexts/AuthContext';
+import '@testing-library/jest-dom/extend-expect';
 
-// test('renders learn react link', () => {
-//   const component = render(
-//     <AuthContext>
-//       <App />
-//     </AuthContext>,
-//     {
-//       value: {
-//         user: {},
-//         userId: {},
-//         authState: {},
-//       },
-//     },
-//   );
+test('should display basic elements.', () => {
+  render(
+    <AuthProvider value={{ user: null }}>
+      <App />
+    </AuthProvider>,
+  );
 
-//   expect(component).toBeInTheDocument();
-// });
+  const homeButton = screen.getByText('minimo');
+  expect(homeButton).toBeInTheDocument();
+
+  const startButton = screen.getByText('Get Started');
+  expect(startButton).toBeInTheDocument();
+});
