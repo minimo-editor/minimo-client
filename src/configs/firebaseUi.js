6e362fc4c92f48b8036f3301cfde71b278e09dd6
minwooKim <vvee1253@gmail.com>
2021-06-10 16:43:14 +0900

    Seperate uiConfig from login

---
+++ src/configs/firebaseUi.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+const firebaseUiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false,
+  },
+};
+
+export default firebaseUiConfig;
