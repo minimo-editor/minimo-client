cb4dcb9d149e118e12c950afd19d8cb97edf0e00
minwooKim <vvee1253@gmail.com>
2021-06-10 10:36:59 +0900

    Add test: App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+// import { AuthContext } from '../../contexts/AuthContext';
+// import '@testing-library/jest-dom/extend-expect';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const component = render(
+//     <AuthContext>
+//       <App />
+//     </AuthContext>,
+//     {
+//       value: {
+//         user: {},
+//         userId: {},
+//         authState: {},
+//       },
+//     },
+//   );
+
+//   expect(component).toBeInTheDocument();
+// });
