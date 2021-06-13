ca8b292310da86a7e773705cb339da2166192c05
minwooKim <vvee1253@gmail.com>
2021-06-13 18:58:21 +0900

    Add test: getValidTexts

---
+++ src/utils/getValidTexts.test.js
@@ -0,0 +1,14 @@
+import getValidTexts from './getValidTexts';
+
+test('should return valid texts', () => {
+  const invalidText = 'a b c #';
+  const validText = 'abc';
+
+  expect(getValidTexts(invalidText)).toBe(validText);
+});
+
+test('should return same texts if given valid texts', () => {
+  const validText = 'abcdef';
+
+  expect(getValidTexts(validText)).toBe(validText);
+});
