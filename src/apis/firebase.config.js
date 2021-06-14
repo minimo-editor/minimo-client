a7b65c7e8ee1c0f8fd791ca0c027925af21f7470
minwooKim <vvee1253@gmail.com>
2021-06-14 15:40:07 +0900

    Delete firebase.config

--- src/apis/firebase.config.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-});
-
-export default firebase;
