94edffea67709ed1d6f41257cc8f5fe253e42bcb
minwooKim <vvee1253@gmail.com>
2021-06-16 20:28:31 +0900

    Refactor AnimateBlocks

--- src/components/AnimateBlocks/AnimateBlocks.js
+++ src/components/AnimateBlocks/AnimateBlocks.js
@@ -1,11 +1,11 @@
 import {
-  useEffect,
   useState,
   useLayoutEffect,
   Children,
 } from 'react';
 import usePrevious from '../../hooks/usePrevious';
 import calculateBoundingBoxes from '../../utils/calculateBoundingBoxes';
+import delay from '../../utils/delay';
 
 export default function AnimateBlocks({ children }) {
   const [boundingBox, setBoundingBox] = useState({});
@@ -22,11 +22,11 @@ export default function AnimateBlocks({ children }) {
     setPrevBoundingBox(prevBoundBox);
   }, [prevChildren]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const hasPrevBoundingBox = Object.keys(prevBoundingBox).length;
 
     if (hasPrevBoundingBox) {
-      Children.forEach(children, (child) => {
+      Children.forEach(children, async (child) => {
         const domNode = child.ref.current;
         const firstBox = prevBoundingBox[child.key];
         const lastBox = boundingBox[child.key];
@@ -38,15 +38,11 @@ export default function AnimateBlocks({ children }) {
         const changeInY = firstBox.top - lastBox.top;
 
         if (changeInY) {
-          requestAnimationFrame(() => {
-            domNode.style.transform = `translateY(${changeInY}px)`;
-            domNode.style.transition = 'transform 0s';
-
-            requestAnimationFrame(() => {
-              domNode.style.transform = '';
-              domNode.style.transition = 'transform 300ms';
-            });
-          });
+          domNode.style.transform = `translateY(${changeInY}px)`;
+          domNode.style.transition = 'transform 0s';
+          await delay();
+          domNode.style.transform = 'none';
+          domNode.style.transition = 'transform 300ms';
         }
       });
     }
